c2ebef3a0ffde9a1050b8a4ab17ebcc6b40d9692
Pavan23052003 <pavansyamala2003@gmail.com>
2024-09-25 03:41:58 +0530

    Changed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup 
+
+setup(
+    name= 'MLOPS with Validation',
+    version= '0.0.0.1',
+    author= 'Kasa Pavan',
+    author_email= 'pavankasa67@gmail.com',
+    description = 'This ia the begining of the MLOPS projects',
+    url= 'https://github.com/Pavansyamala/ClassificationHyperparametrMLOPS.git'
+)
